78b99f26a0b31b88985ccdafd100383e426f538a
anisahfrh <anisahfarah28@gmail.com>
2023-12-27 15:46:45 +0700

    add log

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import cv2
+import sys
+import time
 import torch
 import socket
 import easyocr
 import uvicorn
 import numpy as np
+from loguru import logger
 from fastapi import FastAPI, UploadFile, File
 
 reader = easyocr.Reader(['en'])
+logger.add("logs/app.log", rotation="500 MB")
 
 app = FastAPI()
 
@@ -36,16 +40,25 @@ async def predict(imagefile: UploadFile = File(...)):
     received request from client and process the image
     :return: dict of width and points
     """
-    image_data = await imagefile.read()
-    image = cv2.imdecode(np.frombuffer(image_data, np.uint8), cv2.IMREAD_UNCHANGED)
-    results = recognition(image)
+    try:
+        start = time.time()
+        logger.info(f"Request received by container ID: {socket.gethostname()}")
+        image_data = await imagefile.read()
+        image = cv2.imdecode(np.frombuffer(image_data, np.uint8), cv2.IMREAD_UNCHANGED)
+        results = recognition(image)
+        time_taken = time.time() - start
+        logger.info(f"Request processed in {time_taken:.4f} seconds")
 
-    return {
-        "container ID": socket.gethostname(),
-        "using_gpu": torch.cuda.is_available(),
-        "results": results
-    }
+        return {
+            "container_ID": socket.gethostname(),
+            "using_gpu": torch.cuda.is_available(),
+            "results": results
+        }
+    except:
+        logger.error(f"Error processing request: {str(e)}")
+        return {"error": str(e)}
 
 if __name__=="__main__":
-    uvicorn.run(app,host="0.0.0.0",port=8000)
+    logger.add(sys.stdout, level="INFO", format="<green>{time}</green> <level>{message}</level>")
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_config=None)
 
